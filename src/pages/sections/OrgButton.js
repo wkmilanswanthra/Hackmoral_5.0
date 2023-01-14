3581f634e053520f40f4fcce03c5ab47175cb504
Sampath_Kumara <84168369+sampathhkumara@users.noreply.github.com>
2023-01-14 13:51:03 +0530

    chnaged,jn,jb, the color scheme and the hero sectioni background

--- src/pages/sections/OrgButton.js
+++ src/pages/sections/OrgButton.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const OrgButton = () => {
     return (
-        <div style={{ "backgroundColor": "#1e0205", "position": "relative", "padding": "50px 0" }}>
+        <div style={{ "backgroundColor": "#1e0205", "position": "relative", "padding": "50px 0",  "font-family": "'Roboto Mono', monospace" }}>
             <center>
                 <Link to="/OrganizingCommitte" className=" mt-3 btn text-white" style={{ "backgroundColor": "#D90707" }}>Organizing Committe</Link>
             </center>
